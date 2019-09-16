9c3b57afbf569ee14c71a4e360561c7d843e62e1
Sonia Garcia <43370296+SoniaRuiz@users.noreply.github.com>
2019-09-16 17:02:03 +0100

    Second version
    Updated:
    - Log output
    - Comments added
    - Parameters management improved

--- aws_check_integrity.sh
+++ aws_check_integrity.sh
@@ -1,47 +1,81 @@
 #!/bin/bash
 #echo "First arg: $1"
 #echo "Second arg: $2"
+#echo "Third arg: $3"
 
-# The user must be already connected to S3
+#########################
+## Prerrequisites
+########################
 
-# Receive parameters
-base_folder="$1"
-local_folder="$1"
-bucket_name="$2"
-aws_root_folder="$3"
+# 1. The user must be already connected to S3
+# 2. The local directory must be sincronised with AWS by using the "sync" command
 
-#Create log file
+#########################
+## Receive parameters
+#########################
+
+if [ $# -ne  3 ]; then
+	printf "\n"
+	echo "ERROR: Please pass arguments."
+	printf "Usage :\n aws_check_integrity.sh <local_path> <bucket_name> <folder>\n\t- local_path: local path where all uploaded files are currently stored. For example: /data/nucCyt/raw_data/.\n\t- bucket_name: the name of the S3 bucket we want to check. For example: nuccyt.\n\t- folder: the name of the root folder on the S3 bucket. In case there is not any folder in the root, this parameter will be a slash (/) indicating the root path. Example, raw_data/.\n\n"
+	exit -1
+elif [ ! -d "$1" ]; then
+	printf "\n"
+	echo "ERROR. No such directory exist: "$1
+	printf "\n"
+	exit -2
+else
+	# This variable behaves as a constant. It stores the root path where all files are stored in AWS.
+	base_folder="$1"
+	bucket_name="$2"
+	aws_base_folder="$3"
+fi
+
+########################
+## Create log file
+########################
 log_file=S3_integrity_log
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 log_file=$log_file.$current_time.txt
 echo "Log filename: " "$log_file"
 
-# Loop through the directory
+########################
+## Main function
+########################
 function upload_s3
 {
-	#echo $base
+	# Receive parameters
 	local_folder="$1"
-	bucket_name="$2"
-	base="$3"
+
+	## Declare variables
 	etag_value=""
 
-	for file in "$local_folder"/*; do
-		#echo $file
-		if [ -d "$file" ]; then
-			#echo "Directory"
-			directory="$(basename $file)"
-			upload_s3 "$file" "$bucket_name" $base/$directory
-		else
+	##########################
+	## Loop through the files stored in the local folder
+	#########################
 
+	for file in "$local_folder"/*; do
+
+
+		if [ -d "$file" ]; then
+			## THE OBJECT IS A DIRECTORY
+
+			upload_s3 "$file"
+
+		elif [ -f "$file" ]; then
 			## THE OBJECT IS A FILE
+
+			# Remove the base folder from the local file path - so we could have the AWS path corresponding to the current local file
                         aws_file_path=${file#"$base_folder"}
 
-			## Remove the first first / character if exists
+			## Remove the first first "/" character if it exists
 			if [[ $aws_file_path == /* ]]; then
 				aws_file_path=${aws_file_path#"/"}
 			fi
-			aws_file_path=$aws_root_folder$aws_file_path
-                        #echo $aws_file_path
+
+			# Build the AWS path corresponding to the current local file
+			aws_file_path=$aws_base_folder$aws_file_path
+			#echo $aws_file_path
 
 			## Check the file size and generate the ETag value
 			file_size="$(stat --printf="%s" $file)"
@@ -61,13 +95,12 @@ function upload_s3
   				echo "CORRECT:"$aws_file_path" - "$etag_value >> $log_file
 			else
 				echo $aws_file_path": ERROR"
-				echo "ERROR:"$aws_file_path" - "$etag_value  >> $log_file
+				echo "ERROR:"$aws_file_path" - "$etag_value" - "$aws_response  >> $log_file
 			fi
-
 		fi
 	done
 }
 
-
-upload_s3 "$local_folder" "$bucket_name"
+## Request the function
+upload_s3 "$1"
 
