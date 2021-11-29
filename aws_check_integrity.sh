799082a70973b375f68a56bd0cfa3b4019f44d89
SoniaRuiz <s.ruiz@ucl.ac.uk>
2021-11-29 17:37:28 +0000

    - blank spaces ignored in file and directory names. - the '/' root path is replaced by ''

--- aws_check_integrity.sh
+++ aws_check_integrity.sh
@@ -10,6 +10,7 @@
 # 1. The user must be already connected to S3
 # 2. The local directory must be sincronised with AWS by using the "sync" command
 
+
 #########################
 ## Receive parameters
 #########################
@@ -24,6 +25,11 @@ elif [ ! -d "$1" ]; then
 	echo "ERROR. No such directory exist: "$1
 	printf "\n"
 	exit -2
+elif [ $3 == "/" ]; then
+	## The 3rd parameter corresponds to the root folder. We change it to an empty string for compatibility.
+	base_folder="$1"
+        bucket_name="$2"
+        aws_base_folder=""
 else
 	# This variable behaves as a constant. It stores the root path where all files are stored in AWS.
 	base_folder="$1"
@@ -31,20 +37,25 @@ else
 	aws_base_folder="$3"
 fi
 
+
 ########################
 ## Create log file
 ########################
+
 if [ ! -d ./logs ]; then
   mkdir -p ./logs;
 fi
+
 log_file=./logs/S3_integrity_log
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 log_file=$log_file.$current_time.$2.txt
 echo "Log filename: " "$log_file"
 
+
 ########################
 ## Main function
 ########################
+
 function upload_s3
 {
 	# Receive parameters
@@ -53,23 +64,22 @@ function upload_s3
 	## Declare variables
 	etag_value=""
 
-	##########################
-	## Loop through the files stored in the local folder
-	#########################
+	################################################
+	## Loop through the files from the local folder
+	################################################
 
-	for file in "$local_folder"/*; do
+	for i in "$local_folder"/*
+	do
 
-
-		if [ -d "$file" ]; then
+		if [ -d "$i" ]; then
 			## THE LOCAL OBJECT IS A DIRECTORY
+			upload_s3 "$i"
 
-			upload_s3 "$file"
-
-		elif [ -f "$file" ]; then
+		elif [ -f "$i" ]; then
 			## THE LOCAL OBJECT IS A FILE
 
 			# Remove the base folder from the local file path to have the AWS path corresponding to the current local file
-                        aws_file_path=${file#"$base_folder"}
+			aws_file_path=${i#"$base_folder"}
 
 			## Remove the first first "/" character if it exists
 			if [[ $aws_file_path == /* ]]; then
@@ -78,24 +88,30 @@ function upload_s3
 
 			# Build the AWS path corresponding to the current local file
 			aws_file_path=$aws_base_folder$aws_file_path
-			#echo $aws_file_path
 
 			## Check the file size and generate the ETag value
-			file_size="$(stat --printf="%s" $file)"
+			file_size="$(stat --printf="%s" "$i")"
 			if [ "$file_size" -lt 8000000 ]; then
-				#echo $file_size
-				etag_value="$(md5sum ${file} | awk '{ print $1 }')"
+				etag_value="$(md5sum "$i" | awk '{ print $1 }')"
 			else
-				etag_value="$(./s3md5/s3md5 8 $file)"
+				etag_value="$(./s3md5/s3md5 8 "$i")"
 			fi
+
+			#################################################
+			## Variables that might be useful to double-check
+			#################################################
 			#echo $etag_value
+			#echo "$aws_file_path"
+			#echo $bucket_name
+			#echo $file_size
+
 
 			## Compare the AWS file ETag value with the one we have generated from the local file
-			aws_response="$(aws s3api head-object --bucket $bucket_name --key $aws_file_path --if-match $etag_value)"
+			aws_response="$(aws s3api head-object --bucket $bucket_name --key "$aws_file_path" --if-match $etag_value)"
 			## Save results in a log file
 			if [[ $aws_response == *"ETag"* ]]; then
 				echo $aws_file_path": CORRECT"
-  				echo "CORRECT:"$aws_file_path" - "$etag_value >> $log_file
+				echo "CORRECT:"$aws_file_path" - "$etag_value >> $log_file
 			else
 				echo $aws_file_path": ERROR"
 				echo "ERROR:"$aws_file_path" - "$etag_value" - "$aws_response  >> $log_file
